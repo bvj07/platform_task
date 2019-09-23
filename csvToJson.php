4f529d23342e4b8d69d346699c2e73f07c7448dd
Bobby <bvj07@yahoo.co.uk>
2019-09-23 03:21:27 +0100

    Update csvToJson.php

--- csvToJson.php
+++ csvToJson.php
@@ -2,10 +2,10 @@
 require_once 'functions.php';
 
 // store the file path in a variable
-$file = "products.csv";
+$csvFile = "products.csv";
 
 // get the contents of the csv file
-$csvData = file_get_contents($file);
+$csvData = file_get_contents($csvFile);
 
 // convert the csv data into an array
 $productData = array_map("str_getcsv", explode("\n", $csvData));
@@ -15,10 +15,10 @@ foreach ($productData as $key => $val) {
        
     // store the trimmed values into variables
     $sku    = trim($val[0]);
-    $plu    = trim($val[1]);
-    $name   = trim($val[2]);
-    $size   = trim($val[3]);
-    $sort   = trim($val[4]);
+    $plu    = trim(@$val[1]);
+    $name   = trim(@$val[2]);
+    $size   = trim(@$val[3]);
+    $sort   = trim(@$val[4]);
     
     // create array of required data for the 'addSize' function
     $sizeData = array(
@@ -41,8 +41,14 @@ foreach ($products as $key => $val){
     $products[$key]['sizes'] = array_values(array_filter($val['sizes']));
 }
 
-//print_r($products);
-
 // create the json object
 $json = json_encode($products);
-echo $json;
+
+// open or create a json text file and write the json object to it as a string
+$jsonFile = 'jsonData.txt';
+$fh = fopen($jsonFile, 'w') or die("can't open file");
+fwrite($fh, $json);
+fclose($fh);
+
+//print_r($products);
+//echo $json;
